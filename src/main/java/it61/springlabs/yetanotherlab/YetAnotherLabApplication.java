47b14d6dd0d824af813f349250c82ce15b0dc294
Kirill Lesohorskyi <lesogor.kirill@gmail.com>
2019-02-17 17:34:01 +0200

    Added basic spring

---
+++ src/main/java/it61/springlabs/yetanotherlab/YetAnotherLabApplication.java
@@ -0,0 +1,13 @@
+package it61.springlabs.yetanotherlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YetAnotherLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it61/springlabs/yetanotherlab/YetAnotherLabApplicationTests.java
@@ -0,0 +1,16 @@
+package it61.springlabs.yetanotherlab;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class YetAnotherLabApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
